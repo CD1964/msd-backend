a7477d36d8a506dedb53d28234cf6b79d66599f0
CD1964 <carlfrancisd@gmail.com>
2026-03-30 15:00:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-app.use(cors());
-app.use(express.json());
+    })
+    });
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+carlfdawson@Carls-MacBook-Air Downloads % cat server.js | pbcopy
+carlfdawson@Carls-MacBook-Air Downloads % 
